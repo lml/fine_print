a7cb523dd35b40747f1fd66def0f231a84dbd6e6
Dante Soares <dms3@rice.edu>
2015-03-25 16:15:36 -0500

    Added coveralls gem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
 ENV['RAILS_ENV'] ||= 'test'
 
+require 'coveralls'
+Coveralls.wear!('rails')
+
 require File.expand_path('../dummy/config/environment.rb',  __FILE__)
 require 'rspec/rails'
 require 'factory_girl_rails'
