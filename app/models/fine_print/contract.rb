869b53b8d5079539d289cceba085f8068fcb884f
Joe Moore <joe@wolf-solutions.tech>
2018-06-13 02:48:49 -0400

    Updating before action abort for Rails 5

--- app/models/fine_print/contract.rb
+++ app/models/fine_print/contract.rb
@@ -63,6 +63,7 @@ module FinePrint
     def no_signatures
       return if signatures.empty?
       errors.add(:base, I18n.t('fine_print.contract.errors.already_signed'))
+      throw(:abort)
       false
     end
 
